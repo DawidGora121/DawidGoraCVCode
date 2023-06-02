e13c23e0fdc9a06e15e66f202a5cde659669a3be
DawidGora121 <133766029+DawidGora121@users.noreply.github.com>
2023-06-02 14:08:17 +0200

    Add files via upload

---
+++ RandomAttackAudio.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAttackAudio : MonoBehaviour
+{
+    public AudioSource zombieAttack1;
+    public AudioSource zombieAttack2;
+    public AudioSource zombieAttack3;
+
+
+   public void AttackAudio()
+    {
+        float randomNum = Random.Range(0, 3);
+
+        if(randomNum == 0)
+        {
+            zombieAttack1.Play();
+            zombieAttack1.pitch = Random.Range(0.8f, 1.5f);
+            zombieAttack1.volume = Random.Range(0.8f, 1f);
+        }
+        if (randomNum == 1)
+        {
+            zombieAttack2.Play();
+            zombieAttack2.pitch = Random.Range(0.8f, 1.5f);
+            zombieAttack2.volume = Random.Range(0.8f, 1f);
+        }
+        if (randomNum == 2)
+        {
+            zombieAttack3.Play();
+            zombieAttack3.pitch = Random.Range(0.8f, 1.5f);
+            zombieAttack3.volume = Random.Range(0.8f, 1f);
+        }
+
+
+    }
+
+    void Update()
+    {
+       
+    }
+   
+}
---
+++ RandomHitZombieAudi.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHitZombieAudi : MonoBehaviour
+{
+    public AudioSource zombieHitAudio1;
+    public AudioSource zombieHitAudio2;
+    public AudioSource zombieHitAudio3;
+
+
+    public void RandomHitAudio()
+    {
+        float randomNum = Random.Range(0, 3);
+
+        if (randomNum == 0)
+        {          
+            zombieHitAudio1.pitch = Random.Range(0.8f, 1.1f);
+            zombieHitAudio1.volume = Random.Range(0.8f, 1f);
+            zombieHitAudio1.Play();
+        }
+        if (randomNum == 1)
+        {
+            zombieHitAudio2.pitch = Random.Range(0.8f, 1.1f);
+            zombieHitAudio2.volume = Random.Range(0.8f, 1f);
+            zombieHitAudio2.Play();
+        }
+        if (randomNum == 2)
+        {           
+            zombieHitAudio3.pitch = Random.Range(0.8f, 1.5f);
+            zombieHitAudio3.volume = Random.Range(0.8f, 1.1f);
+            zombieHitAudio3.Play();
+        }
+
+    }
+
+
+}
